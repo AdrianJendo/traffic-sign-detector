3cc9d4900028559c7e00c5e89ed17d71ababf222
Adrian Jendo <ajendo@uwaterloo.ca>
2022-08-18 17:41:39 -0400

    Add train.py

---
+++ train-scripts/test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.models import load_model
+
+# accuracy on test dataset
+model = load_model("traffic_sign_classifier.h5")
+
+y_test = pd.read_csv("./data/Test.csv")
+labels = y_test["ClassId"].values
+imgs = y_test["Path"].values
+
+data = []
+
+for img in imgs:
+    image = Image.open("./data/{}".format(img))
+    image = image.resize((30, 30))
+    data.append(np.array(image))
+
+x_test = np.array(data)
+pred = np.argmax(model.predict(x_test), axis=-1)
+
+print(labels)
+print(pred)
+
+print(accuracy_score(labels, pred))
--- train-scripts/train.py
+++ train-scripts/train.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
 from PIL import Image
 import os
 from sklearn.model_selection import train_test_split
